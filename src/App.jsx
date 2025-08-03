3074630e50d80d79e552f72a396d438dcd5c273c
Nassim <nassim.elhaddad2004@gmail.com>
2025-08-03 15:15:43 +0200

    Initial commit - GSAP website project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { ScrollTrigger, SplitText} from "gsap/all";
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+import React from 'react';
+
+const App = () => {
+    return (
+        <div> <h1 className="text-3xl text-indigo-300"> Hello world !</h1> </div>
+    )
+}
+export default App;
